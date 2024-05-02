62797e7f6ad2fc70f1a21ddd4d725663973047f3
danish <mdanishateeq@gmail.com>
2024-05-02 17:19:07 +0500

    adding PalindromeNumber.java and it's class

---
+++ PalindromeNumber.java
@@ -0,0 +1,21 @@
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        
+        String s = String.valueOf(x);
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new PalindromeNumber().isPalindrome(-212));
+    }
+}
