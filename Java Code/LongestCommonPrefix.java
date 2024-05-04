12e675b0b46a19f7f2e6bad8d5d96ff43f22bbd9
danish <mdanishateeq@gmail.com>
2024-05-04 19:10:29 +0500

    update LongestCommonPrefix.java file

--- Java Code/LongestCommonPrefix.java
+++ Java Code/LongestCommonPrefix.java
@@ -1,8 +1,29 @@
 public class LongestCommonPrefix {
 
-    public String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {
+        int minLength = Integer.MAX_VALUE;
+        StringBuilder prefix = new StringBuilder();
 
+        for (String s : strs) {
+            minLength = Math.min(minLength, s.length());
+        }
 
-        return null;
+        for (short i; i < minLength; i++) {
+            char c = strs[0].charAt(i);
+            for (String s : strs) {
+                if (s.charAt(i) != c) {
+                    return prefix.toString();
+                }
+            }
+            prefix.append(c);
+        }
+
+        return prefix.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = { "flower", "flow", "flight" };
+        System.out.println(longestCommonPrefix(strs));
     }
 }
