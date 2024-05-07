ed82115765d2bc7f6b16ccfe41372a6e16dcc019
danish <mdanishateeq@gmail.com>
2024-05-07 21:13:34 +0500

    creating RemoveElement.java file

---
+++ Java Code/RemoveElement.java
@@ -0,0 +1,14 @@
+
+public class RemoveElement {
+public static void main(String[] args) {
+    int []nums = {1,2,3,4,1,2,3,4};
+
+    System.out.println(removeElement(nums, 2)); 
+}
+
+public static int removeElement(int[] nums, int val) {
+    
+    return val;    
+}
+    
+}
