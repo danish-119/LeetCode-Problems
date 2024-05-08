3eb8324d3c0022527d8497a5e7472f269603c761
danish <mdanishateeq@gmail.com>
2024-05-08 21:59:50 +0500

    updating searchInserPosition.c file

--- C Code/searchInsertPosition.c
+++ C Code/searchInsertPosition.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main(){
+short searchInsertPosition(int nums[], int numsSize, int target);
 
+int main(){
+    int nums[] = {1, 3, 5, 7};
+    short size = sizeof(nums) / sizeof(nums[0]);
+
+    printf("%d", searchInsertPosition(nums, size, 6));
+    
     return 0;
+}
+
+short searchInsertPosition(int nums[], int numsSize, int target){
+    short count = 0;
+
+    for(short i = 0; i < numsSize && nums[i] < target; i++){
+        if(nums[i] <= target){
+            count++;
+        }
+    }
+
+    return count;
 }
