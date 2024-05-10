1ba6ca64dfa2fce2280269a755c49412ebd2af30
danish <mdanishateeq@gmail.com>
2024-05-10 09:18:20 +0500

    updating containsDuplicate.c file

--- C Code/containsDuplicate.c
+++ C Code/containsDuplicate.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(){
+bool containsDuplicate(int nums[], int numsSize);
+
+int main()
+{
+    int nums[] = {1, 2, 3, 1};
+    short size = sizeof(nums) / sizeof(nums[0]);
+
+    printf("%d", containsDuplicate(nums, size));
 
     return 0;
+}
+
+bool containsDuplicate(int nums[], int numsSize)
+{
+    for (short i = 0; i < numsSize - 1; i++)
+    {
+        for (short j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] == nums[j])
+                return true;
+        }
+    }
+    return false;
 }
