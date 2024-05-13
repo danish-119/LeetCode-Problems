a51b9e09fab8cab9034b16a2f083d1da307d5bc3
danish <mdanishateeq@gmail.com>
2024-05-13 22:56:50 +0500

    updating ContainsDuplicate2.java file

--- Java Code/ContainsDuplicate2.java
+++ Java Code/ContainsDuplicate2.java
@@ -1,3 +1,26 @@
+import java.util.HashMap;
+
 public class ContainsDuplicate2 {
-    
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // Map to store the last index of each number
+        HashMap<Integer, Integer> numIndexMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            // Check if the number is already in the map
+            if (numIndexMap.containsKey(num)) {
+                // If the absolute difference between current index and the last index is less
+                // than or equal to k, return true
+                if (i - numIndexMap.get(num) <= k) {
+                    return true;
+                }
+            }
+            // Update the index of the current number
+            numIndexMap.put(num, i);
+        }
+
+        // No such pair found, return false
+        return false;
+    }
 }
