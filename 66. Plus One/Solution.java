0fb7fce157ca3238a3e748364ed0d4cdc5ed5420
danish <mdanishateeq@gmail.com>
2024-06-26 15:07:10 +0500

    _

--- 66. Plus One/Solution.java
+++ 66. Plus One/Solution.java
@@ -1,5 +1,15 @@
 
 public class Solution {
+    public static void main(String[] args) {
 
-    
+        int[] digits = { 1, 2, 3 };
+        plusOne(digits);
+
+        System.out.println(plusOne(digits));
+    }
+
+    public static int[] plusOne(int[] digits) {
+
+        return digits;
+    }
 }
