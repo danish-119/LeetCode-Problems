5e3f165b050ee26b7cb73ced28fe579dd4215f8a
danish <mdanishateeq@gmail.com>
2024-06-26 15:52:42 +0500

    _

--- 66. Plus One/Solution.java
+++ 66. Plus One/Solution.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) {
 
-        int[] digits = { 9, 9, 9 };
+        int[] digits = { 1, 9, 9 };
 
         for (int i : plusOne(digits)) {
             System.out.println(i);
@@ -12,25 +12,27 @@ public class Solution {
 
     public static int[] plusOne(int[] digits) {
 
-        for (int i = digits.length - 1; i >= 1; i--) {
+        for (int i = digits.length - 1; i >= 0; i--) {
 
             digits[i] = digits[i] + 1;
 
             if (digits[i] == 10) {
                 digits[i] = 0;
-            } else if (digits[0] == 10) {
-                digits[0] = 0;
-                // int []nums = new int[digits.length + 1];
-                // nums[0] = 1;
-                ArrayList<Integer> nums = new ArrayList<>();
-                nums.add(1);
-                nums.addAll(digits);
-                return nums.toArray();
             } else {
                 return digits;
             }
 
         }
+
+        if (digits[0] == 0) {
+            int[] arr = new int[digits.length + 1];
+            arr[0] = 1;
+            for (int j = 1; j < arr.length; j++) {
+                arr[j] = digits[j - 1];
+            }
+            return arr;
+        }
+
         return digits;
     }
 }
