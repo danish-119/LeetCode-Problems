52074c1c91621e5efcfe90eadfc7b2629477663e
Muhammad Danish <mdanishateeq@gmail.com>
2024-07-09 10:04:40 +0500

    _

---
+++ 59. Spiral Matrix II/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public static void main(String[] args) {
+        int arr[][] = generateMatrix(3);
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        int num = 1;
+        
+        while (num <= n * n) {
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                matrix[bottom][i] = num++;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                matrix[i][left] = num++;
+            }
+            left++;
+        }
+        return matrix;
+    }
+}
